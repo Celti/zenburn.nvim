ce812235189d01bde64ff0788cd7acb90e15cd32
Celti Burroughs <celti@celti.name>
2024-03-04 02:36:00 -0700

    treesitter semantic highlights

--- lua/zenburn/highlights/treesitter.lua
+++ lua/zenburn/highlights/treesitter.lua
@@ -1,124 +1,76 @@
-local c = require("zenburn.palette")
 return {
-	TSNone = {},
-	TSError = {},
-	TSTitle = { fg=c.Title.fg },
-	TSLiteral = { fg=c.String.fg },
-	TSURI = { underline=true },
-	TSVariable = { fg=c.Normal.fg },
-	TSPunctDelimiter = { fg=c.Delimiter.fg },
-	TSPunctBracket = { fg=c.Delimiter.fg },
-	TSPunctSpecial = { fg=c.Delimiter.fg },
-	TSConstant = { fg=c.Constant.fg },
-	TSConstBuiltin = { fg=c.Special.fg },
-	TSConstMacro = { fg=c.Define.fg },
-	TSString = { fg=c.String.fg },
-	TSStringRegex = { fg=c.String.fg },
-	TSStringEscape = { fg=c.SpecialChar.fg },
-	TSCharacter = { fg=c.Character.fg },
-	TSNumber = { fg=c.Number.fg },
-	TSBoolean = { fg=c.Boolean.fg },
-	TSFloat = { fg=c.Float.fg },
-	TSFunction = { fg=c.Function.fg },
-	TSFuncBuiltin = { fg=c.Special.fg },
-	TSFuncMacro = { fg=c.Macro.fg },
-	TSParameter = { fg=c.ModeMsg.fg },
-	TSParameterReference = { fg=c.ModeMsg.fg },
-	TSMethod = { fg=c.Include.fg },
-	TSField = { fg=c.Identifier.fg },
-	TSProperty = { fg=c.Identifier.fg },
-	TSConstructor = { fg=c.Include.fg },
-	TSAnnotation = { fg=c.PreProc.fg },
-	TSAttribute = { fg=c.PreProc.fg },
-	TSNamespace = { fg=c.Include.fg },
-	TSConditional = { fg=c.Conditional.fg },
-	TSRepeat = { fg=c.Repeat.fg },
-	TSLabel = { fg=c.Label.fg },
-	TSOperator = { fg=c.Operator.fg },
-	TSKeyword = { fg=c.Keyword.fg },
-	TSKeywordFunction = { fg=c.Keyword.fg },
-	TSKeywordOperator = { fg=c.Operator.fg },
-	TSKeywordReturn = { fg=c.Float.fg },
-	TSException = { fg=c.Exception.fg },
-	TSType = { fg=c.Delimiter.fg },
-	TSTypeBuiltin = { fg=c.Delimiter.fg },
-	TSInclude = { fg=c.Include.fg },
-	TSVariableBuiltin = { fg=c.Number.fg },
-	TSText = {},
-	TSStrong = { bold=true, underline=true },
-	TSEmphasis = { bold=true },
-	TSUnderline = { underline=true },
-	TSComment = { fg=c.Comment.fg },
-	TSStructure = { fg=c.Structure.fg },
-	TSTag = { fg=c.Tag.fg },
-	TSTagDelimiter = { fg=c.Tag.fg },
+	-- Text
+	['@markup.strong'] =        { fg = "#efdcbc", bold = true },          -- bold text
+	['@markup.italic'] =        { fg = "#efdcbc", italic = true },        -- italic text
+	['@markup.strikethrough'] = { fg = "#8f8f8f", strikethrough = true }, -- struck-through text
+	['@markup.underline'] =     { fg = "#efdcbc", underline = true },     -- underlined text (only for literal underline markup!)
+
+	['@markup.heading'] =      { link = 'Title' },                        -- headings, titles (including markers)
+	['@markup.heading.1'] =    { fg = "#8cd0d3", bold = true },           -- top-level heading
+	['@markup.heading.2'] =    { fg = "#c0bed1", bold = true },           -- section heading
+	['@markup.heading.3'] =    { fg = "#efefef", bold = true },           -- subsection heading
+	['@markup.heading.4'] =    { fg = "#f0dfaf", bold = true },           -- and so on
+	['@markup.heading.5'] =    { fg = "#ffcfaf", bold = true },           -- and so forth
+	['@markup.heading.6'] =    { fg = "#dca3a3", bold = true },           -- six levels ought to be enough for anybody
+
+	['@markup.quote'] =        { link = 'Special' },                      -- block quotes
+	['@markup.math'] =         { link = 'Operator' },                     -- math environments (e.g. `$ ... $` in LaTeX)
+
+	['@markup.link'] =         { fg = '#8f8f8f' },                        -- text references, footnotes, citations, etc.
+	['@markup.link.label'] =   { fg = '#efdcbc' },                        -- link, reference descriptions
+	['@markup.link.url'] =     { fg = '#cc9393', underdotted = true },    -- URL-style links
+
+	['@markup.raw'] =          { link = 'Comment' },                      -- literal or verbatim text (e.g. inline code)
+	['@markup.raw.block'] =    { link = 'Comment' },                      -- literal or verbatim text as a stand-alone block
+
+	['@markup.list']           = { link = 'Delimiter' },                  -- list markers
+	['@markup.list.checked']   = { fg = "#7f9f7f" },                      -- checked todo-style list markers
+	['@markup.list.unchecked'] = { fg = "#dca3a3", bold = true },         -- unchecked todo-style list markers
+
+	-- Miscs
+	['@comment'] =             { link = 'Comment' },
+	['@comment.todo'] =        { link = 'Todo' },
+	['@punctuation'] =         { link = 'Delimiter' },
+
+	-- Constants
+	['@constant'] =            { link = 'Constant' },
+	['@constant.builtin'] =    { link = 'Special' },
+	['@constant.macro'] =      { link = 'Define' },
+	['@keyword.directive'] =   { link = 'Define' },
+	['@string'] =              { link = 'String' },
+	['@string.escape'] =       { link = 'SpecialChar' },
+	['@string.special'] =      { link = 'SpecialChar' },
+	['@character'] =           { link = 'Character' },
+	['@character.special'] =   { link = 'SpecialChar' },
+	['@number'] =              { link = 'Number' },
+	['@boolean'] =             { link = 'Boolean' },
+	['@number.float'] =        { link = 'Float' },
+
+	-- Functions
+	['@function'] =            { link = 'Function' },
+	['@function.builtin'] =    { link = 'Special' },
+	['@function.macro'] =      { link = 'Macro' },
+	['@variable.parameter'] =  { link = 'Identifier' },
+	['@function.method'] =     { link = 'Function' },
+	['@variable.member'] =     { link = 'Identifier' },
+	['@property'] =            { link = 'Identifier' },
+	['@constructor'] =         { link = 'Special' },
+
+	-- Keywords
+	['@keyword.conditional'] = { link = 'Conditional' },
+	['@keyword.repeat'] =      { link = 'Repeat' },
+	['@label'] =               { link = 'Label' },
+	['@operator'] =            { link = 'Operator' },
+	['@keyword'] =             { link = 'Keyword' },
+	['@keyword.exception'] =   { link = 'Exception' },
+
+	['@variable'] =            { link = 'Identifier' },
+	['@type'] =                { link = 'Type' },
+	['@type.definition'] =     { link = 'Typedef' },
+	['@keyword.storage'] =     { link = 'StorageClass' },
+	['@module'] =              { link = 'Identifier' },
+	['@keyword.import'] =      { link = 'Include' },
+	['@keyword.directive'] =   { link = 'PreProc' },
+	['@keyword.debug'] =       { link = 'Debug' },
+	['@tag'] =                 { link = 'Tag' },
 }
--- TSAttribute
--- TSBoolean
--- TSCharacter
--- TSCharacterSpecial
--- TSComment
--- TSConditional
--- TSConstant
--- TSConstBuiltin
--- TSConstMacro
--- TSConstructor
--- TSDebug
--- TSDefine
--- TSError
--- TSException
--- TSField
--- TSFloat
--- TSFunction
--- TSFuncBuiltin
--- TSFuncMacro
--- TSInclude
--- TSKeyword
--- TSKeywordFunction
--- TSKeywordOperator
--- TSKeywordReturn
--- TSLabel
--- TSMethod
--- TSNamespace
--- TSNone
--- TSNumber
--- TSOperator
--- TSParameter
--- TSParameterReference
--- TSPreProc
--- TSProperty
--- TSPunctDelimiter
--- TSPunctBracket
--- TSPunctSpecial
--- TSRepeat
--- TSStorageClass
--- TSString
--- TSStringRegex
--- TSStringEscape
--- TSStringSpecial
--- TSSymbol
--- TSTag
--- TSTagAttribute
--- TSTagDelimiter
--- TSText
--- TSStrong
--- TSEmphasis
--- TSUnderline
--- TSStrike
--- TSTitle
--- TSLiteral
--- TSURI
--- TSMath
--- TSTextReference
--- TSEnvironment
--- TSEnvironmentName
--- TSNote
--- TSWarning
--- TSDanger
--- TSTodo
--- TSType
--- TSTypeBuiltin
--- TSTypeQualifier
--- TSTypeDefinition
--- TSVariable
--- TSVariableBuiltin
