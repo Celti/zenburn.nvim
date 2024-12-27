1d54397f432c0ace6aa46f6a0fcf23d0b3292cae
Celti Burroughs <celti@celti.name>
2024-12-27 10:34:39 -0700

    render-markdown.nvim bg groups

---
+++ lua/zenburn/highlights/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+	['RenderMarkdownH1Bg'] = { link = "DiffChange" },
+	['RenderMarkdownH2Bg'] = { link = "DiffChange" },
+	['RenderMarkdownH3Bg'] = { link = "DiffChange" },
+	['RenderMarkdownH4Bg'] = { link = "DiffChange" },
+	['RenderMarkdownH5Bg'] = { link = "DiffChange" },
+	['RenderMarkdownH6Bg'] = { link = "DiffChange" },
+}
